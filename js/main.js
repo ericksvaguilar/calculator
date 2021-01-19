3e04dd9b8d191ad22c5cd7e6f5dac2f99ed08788
Erick Aguilar <ericksvaguilar@gmail.com>
2021-01-19 15:42:09 -0300

    feat: start functionality

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
-const operandsInput = document.querySelectorAll('[data-operands]')
-const operatorsInput = document.querySelectorAll('[data-operators]')
-const displayOutput = document.querySelector('[data-display]')
-const clearInput = document.querySelector('[data-clear]')
-const equalsInput = document.querySelector('[data-equals]')
+class Calculator {
+  constructor(previousOperandOutputElement, currentOperandOutputElement) {
+    this._previousOperandOutputElement = previousOperandOutputElement
+    this._currentOperandOutputElement = currentOperandOutputElement
+  }
+
+  clear() {
+    this._currentOperandOutputElement = ''
+    this._previousOperandOutputElement = ''
+  }
+}
+
+const operandsInputElement = document.querySelectorAll('[data-operands]')
+const operatorsInputElement = document.querySelectorAll('[data-operators]')
+const previousOperandOutputElement = document.querySelector('[data-previous-operand]')
+const currentOperandOutputElement = document.querySelector('[data-current-operand]')
+const clearInputElement = document.querySelector('[data-clear]')
+const equalsInputElement = document.querySelector('[data-equals]')
+
+const calculator = new Calculator(previousOperandOutputElement, currentOperandOutputElement)
+
+console.log(calculator);
