7f7300acdef5e49038282f07ffdd8e4de8c84f42
Erick Aguilar <ericksvaguilar@gmail.com>
2021-01-19 18:26:22 -0300

    add methods to calculator class

--- js/main.js
+++ js/main.js
@@ -1,13 +1,50 @@
+/*
+
+TO-DO:
+- create get and set to the Calculator class
+
+*/
+
 class Calculator {
   constructor(previousOperandOutputElement, currentOperandOutputElement) {
     this._previousOperandOutputElement = previousOperandOutputElement
     this._currentOperandOutputElement = currentOperandOutputElement
+    this._operator = undefined
+    this.clear()
+  }
+
+  appendNumber(number) {
+    this._currentOperandOutputElement.innerText += number
+  }
+
+  updateDisplay() {
+    this._previousOperandOutputElement.innerText = this._currentOperandOutputElement.innerText
+    this._currentOperandOutputElement.innerText = ''
   }
 
   clear() {
-    this._currentOperandOutputElement = ''
-    this._previousOperandOutputElement = ''
+    this._previousOperandOutputElement.innerText = ''
+    this._currentOperandOutputElement.innerText = ''
   }
+
+  chooseOperator(operator) {
+    this._operator = operator
+  }
+
+  compute() {
+    if (this._currentOperandOutputElement && this._previousOperandOutputElement) {
+      let currentOperandNumber = Number(this._currentOperandOutputElement.value)
+      let previousOperandNumber = Number(this._previousOperandOutputElement.value)
+      let result
+      switch (this._operator) {
+        case '*':
+          result = currentOperandNumber * previousOperandNumber
+      }
+
+    }
+
+  }
+
 }
 
 const operandsInputElement = document.querySelectorAll('[data-operands]')
@@ -19,4 +56,20 @@ const equalsInputElement = document.querySelector('[data-equals]')
 
 const calculator = new Calculator(previousOperandOutputElement, currentOperandOutputElement)
 
-console.log(calculator);
+operandsInputElement.forEach(element => {
+  element.addEventListener('click', element => {
+    calculator.appendNumber(element.target.value)
+  })
+})
+
+operatorsInputElement.forEach(element => {
+  element.addEventListener('click', element => {
+    calculator.chooseOperator(element.target.value)
+    calculator.updateDisplay()
+    calculator.compute()
+  })
+})
+
+clearInputElement.addEventListener('click', () => {
+  calculator.clear()
+})
