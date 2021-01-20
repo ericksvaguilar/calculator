633debb7b443ff8e6a86a7ddb8e0fd894d67a4fb
Erick Aguilar <ericksvaguilar@gmail.com>
2021-01-20 12:22:26 -0300

    refactor main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,3 @@
-/*
-
-TO-DO:
-- create get and set to the Calculator class
-
-*/
-
 class Calculator {
   constructor(previousOperandOutputElement, currentOperandOutputElement) {
     this._previousOperandOutputElement = previousOperandOutputElement
@@ -13,55 +6,77 @@ class Calculator {
     this.clear()
   }
 
+  /* || Getters and Setters */
+  get previousOperand() {
+    return Number(this._previousOperandOutputElement.innerText)
+  }
+
+  get currentOperand() {
+    return Number(this._currentOperandOutputElement.innerText)
+  }
+
+  get operator() {
+    return this._operator
+  }
+
+  set previousOperand(number) {
+    this._previousOperandOutputElement.innerText = number
+  }
+
+  set currentOperand(number) {
+    this._currentOperandOutputElement.innerText = number
+  }
+
+  set operator(operator) {
+    this._operator = operator
+  }
+
+  /* || Methods */
   appendNumber(number) {
-    this._currentOperandOutputElement.innerText += number
+    if (this.currentOperand.toString().length <= 8) {
+      if (this.currentOperand) {
+        this.currentOperand = this.currentOperand.toString() + number
+      } else {
+        this.currentOperand = number
+      }
+    }
   }
 
   updateDisplay() {
-    if (this._currentOperandOutputElement.innerText) {
-      this._previousOperandOutputElement.innerText = this._currentOperandOutputElement.innerText
-      this._currentOperandOutputElement.innerText = ''
+    if (this.currentOperand) {
+      this.previousOperand = this.currentOperand
+      this.currentOperand = ''
     }
   }
 
   clear() {
-    this._previousOperandOutputElement.innerText = ''
-    this._currentOperandOutputElement.innerText = ''
-  }
-
-  chooseOperator(operator) {
-    this._operator = operator
+    this.previousOperand = ''
+    this.currentOperand = ''
+    this.operator = undefined
   }
 
   compute() {
-    let currentOperandNumber = Number(this._currentOperandOutputElement.value)
-    let previousOperandNumber = Number(this._previousOperandOutputElement.value)
-    let result
+    let result = 0;
 
-    if (this._previousOperandOutputElement.innerText && this._currentOperandOutputElement.innerText) {
-      switch (this._operator) {
+    if (this.previousOperand && this.currentOperand) {
+      switch (this.operator) {
         case '*':
-          result = currentOperandNumber * previousOperandNumber
+          result = this.currentOperand * this.previousOperand
           break
         case '-':
-          result = previousOperandNumber - currentOperandNumber
+          result = this.previousOperand - this.currentOperand
           break
         case '/':
-          result = previousOperandNumber / currentOperandNumber
+          result = this.previousOperand / this.currentOperand
           break
         case '+':
-          result = currentOperandNumber + previousOperandNumber
+          result = this.currentOperand + this.previousOperand
           break
-
       }
 
-      this._currentOperandOutputElement.innerText = result
-    } else {
-
+      this.currentOperand = result
     }
-
   }
-
 }
 
 const operandsInputElement = document.querySelectorAll('[data-operands]')
@@ -73,6 +88,7 @@ const equalsInputElement = document.querySelector('[data-equals]')
 
 const calculator = new Calculator(previousOperandOutputElement, currentOperandOutputElement)
 
+/* || Event Listeners */
 operandsInputElement.forEach(element => {
   element.addEventListener('click', element => {
     calculator.appendNumber(element.target.value)
@@ -81,7 +97,7 @@ operandsInputElement.forEach(element => {
 
 operatorsInputElement.forEach(element => {
   element.addEventListener('click', element => {
-    calculator.chooseOperator(element.target.value)
+    calculator.operator = element.target.value
     calculator.compute()
     calculator.updateDisplay()
   })
